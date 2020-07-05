fe254bc7a522def6c0ffdbf47afea272bfa0dcdc
sristi <send2sristi@gmail.com>
2020-07-05 16:58:27 +0530

    angryBackground

--- sketch.js
+++ sketch.js
@@ -7,11 +7,11 @@ var engine, world;
 var box1, pig1,pig3;
 var backgroundImg,platform;
 var bird, slingshot;
-
+var score=0;
 var gameState = "onSling";
 
 function preload() {
-    backgroundImg = loadImage("sprites/bg.png");
+    getgoodbackground(); 
 }
 
 function setup(){
@@ -45,18 +45,26 @@ function setup(){
 }
 
 function draw(){
-    background(backgroundImg);
+    if(backgroundImg){
+    background(backgroundImg);}
+
+
+    textSize(35);
+    fill("white");
+    text("score "+score,width-300,50);
     Engine.update(engine);
     //strokeWeight(4);
     box1.display();
     box2.display();
     ground.display();
     pig1.display();
+    pig1.score();
     log1.display();
 
     box3.display();
     box4.display();
     pig3.display();
+    pig3.score();
     log3.display();
 
     box5.display();
@@ -85,4 +93,17 @@ function keyPressed(){
     if(keyCode === 32){
        // slingshot.attach(bird.body);
     }
+}
+ async function getgoodbackground(){
+   var response= await fetch("https://worldtimeapi.org/api/timezone/Asia/Tokyo");
+   var responseJSON = await response.json();
+   var hour = responseJSON.datetime.slice(11,13);
+   console.log(hour);
+   if(hour>=6 && hour<=18){
+       bg="sprites/bg.png";
+       
+   } else{
+       bg="sprites/bg2.jpg";
+   }
+   backgroundImg=loadImage(bg);
 }
